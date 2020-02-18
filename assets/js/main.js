b448dab637f37763b6128d5d3a68b95829a186ca
patriciax <yively.pa@gmail.com>
2020-02-18 15:27:44 -0400

    home

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+/*
+Title: Main Scripts
+Theme Name: Myass
+Author Name: Yively Alvarez
+Website: http://myass.co/
+*/
+
+$(function () {
+  'use strict'
+
+  $('[data-toggle="offcanvas"]').on('click', function () {
+    $('.offcanvas-collapse').toggleClass('open')
+  })
+})
+
+
+// Full screen pre loader
+// $(window).load(function () {
+//   $("#pre-loader").delay(500).fadeOut(2000);
+//   $(".preload-logo").addClass('zoomOutUp');
+//   $(".loader").addClass('zoomOutDown');
+// });
+
+let ubicationP = window.pageYOffset;
+window.onscroll = function(){
+  let ScrollA = window.pageYOffset;
+  if(ubicationP >= ScrollA){
+    document.getElementById('navbar').style.top = '0';
+  }
+  else{
+    document.getElementById('navbar').style.top = '-100px';
+  }
+  ubicationP = ScrollA;
+}
+
+
+
+// Menú fixed
+$(window).scroll(function () {
+  if ($(document).scrollTop() > 70 && ($(window).width() >= 768)) {
+    $('.navbar-fixed-js').addClass('fixed');
+    //  $('.navbar-fixed-js').addClass('navbar-none');
+    // $("#iso").addClass('img-size').attr('src', 'assets/img/iso.jpg').removeClass('scroll-up');
+
+  } else {
+    $('.navbar-fixed-js').removeClass('fixed');
+    $("#iso").removeClass('img-size').attr('src', 'assets/img/iso-color.png').removeClass('scroll-up');
+    // $('.navbar-fixed-js').addClass('navbar-visi');
+   
+  }
+});
+
+$(".hamburger").on("click", function () {
+  $(this).toggleClass("is-active");
+});
---
+++ assets/js/setting-slick.js
@@ -0,0 +1,130 @@
+$('.main-banner__content').slick({
+  infinite: true,
+  autoplay: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: false,
+  arrows: true,
+  responsive: [{
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        autoplay: true,
+        arrows: false,
+        autoplaySpeed: 1000
+      }
+    }
+  ]
+});
+
+$('.main-client__content').slick({
+  infinite: true,
+  slidesToShow: 5,
+  slidesToScroll: 4,
+  dots: true,
+  arrows: true,
+  responsive: [{
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 1000
+      }
+    }
+  ]
+});
+$('.screen-slider__porfolio').slick({
+  infinite: true,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  dots: true,
+  arrows: true,
+  responsive: [{
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 1000
+      }
+    }
+  ]
+});
+
+
+// $('.main-steps__content').slick({
+
+//   responsive: [{
+
+//       breakpoint: 900,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 1
+//       }
+//     },
+//     {
+//       breakpoint: 600,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         dots: false,
+//         autoplay: true,
+//         arrows: false,
+//         autoplaySpeed: 1000
+//       }
+//     }
+//   ]
+// });
