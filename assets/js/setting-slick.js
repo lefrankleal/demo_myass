22c564fd6959a4d65c0852a7f4fdf8947b462a60
patriciax <yively.pa@gmail.com>
2020-02-18 15:55:42 -0400

    home

--- assets/js/setting-slick.js
+++ assets/js/setting-slick.js
@@ -1,39 +1,39 @@
-$('.main-banner__content').slick({
-  infinite: true,
-  autoplay: false,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  dots: false,
-  arrows: true,
-  responsive: [{
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 900,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: false,
-        autoplay: true,
-        arrows: false,
-        autoplaySpeed: 1000
-      }
-    }
-  ]
-});
+// $('.main-banner__content').slick({
+//   infinite: true,
+//   autoplay: false,
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   dots: false,
+//   arrows: true,
+//   responsive: [{
+//       breakpoint: 1200,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         infinite: true,
+//         dots: false
+//       }
+//     },
+//     {
+//       breakpoint: 900,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1
+//       }
+//     },
+//     {
+//       breakpoint: 600,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         dots: false,
+//         autoplay: true,
+//         arrows: false,
+//         autoplaySpeed: 1000
+//       }
+//     }
+//   ]
+// });
 
 $('.main-client__content').slick({
   infinite: true,
